7c9da764d661a94cf5259affd312c3d2a0a909c0
sao-coding <jeter.nice@gmail.com>
2026-03-11 21:15:37 +0800

    chore: 移除未使用的註解代碼

--- apps/server/src/orpc/index.ts
+++ apps/server/src/orpc/index.ts
@@ -18,3 +18,42 @@ const requireAuth = o.middleware(async ({ context, next }) => {
 });
 
 export const protectedProcedure = publicProcedure.use(requireAuth);
+
+
+// import { ORPCError, os } from "@orpc/server";
+
+// import type { Context } from "./context";
+
+// import { getLogger, type LoggerContext } from '@orpc/experimental-pino'
+
+// // interface ORPCContext extends LoggerContext {} 
+
+
+
+// type ORPCContext = Context & LoggerContext;
+
+// export const o = os.$context<ORPCContext>();
+
+// const withRequestLogging = o.middleware(async ({ context, next }) => {
+//   const logger = getLogger(context);
+//   logger?.info('Processing request');
+
+//   return next();
+// });
+
+// export const publicProcedure = o.use(withRequestLogging);
+
+// // export const publicProcedure = o;
+
+// const requireAuth = o.middleware(async ({ context, next }) => {
+//   if (!context.session?.user) {
+//     throw new ORPCError("UNAUTHORIZED");
+//   }
+//   return next({
+//     context: {
+//       session: context.session,
+//     },
+//   });
+// });
+
+// export const protectedProcedure = publicProcedure.use(requireAuth);
