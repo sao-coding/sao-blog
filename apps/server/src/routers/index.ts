36845136364d58ccbb8c42ebb46f2185789b6c2e
sao-coding <jeter.nice@gmail.com>
2026-03-11 21:18:05 +0800

    feat: 將 commentRouter 添加至 appRouter，並註解掉健康檢查和私有數據處理程序

--- apps/server/src/routers/index.ts
+++ apps/server/src/routers/index.ts
@@ -3,19 +3,21 @@ import type { RouterClient } from "@orpc/server";
 import { protectedProcedure, publicProcedure } from "@/orpc/index";
 import postRouter from "./post";
 import noteRouter from "./note";
+import { commentRouter } from "./comment";
 
 export const appRouter = {
-  healthCheck: publicProcedure.handler(() => {
-    return "OK";
-  }),
-  privateData: protectedProcedure.handler(({ context }) => {
-    return {
-      message: "This is private",
-      user: context.session?.user,
-    };
-  }),
+  // healthCheck: publicProcedure.handler(() => {
+  //   return "OK";
+  // }),
+  // privateData: protectedProcedure.handler(({ context }) => {
+  //   return {
+  //     message: "This is private",
+  //     user: context.session?.user,
+  //   };
+  // }),
   post: postRouter,
   note: noteRouter,
+  comment: commentRouter,
 };
 export type AppRouter = typeof appRouter;
 export type AppRouterClient = RouterClient<typeof appRouter>;
