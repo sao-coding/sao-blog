ae3de62d41d1cdd1fee5f08ff265beb43c56a9c3
sao-coding <jeter.nice@gmail.com>
2026-04-09 23:01:59 +0800

    feat: 新增文章編輯器路由，支持文章創建和編輯功能

--- apps/admin/src/routes/posts/editor/index.tsx
+++ apps/admin/src/routes/posts/editor/index.tsx
@@ -1,12 +1,10 @@
 import { createFileRoute } from '@tanstack/react-router'
 import { PostEditor } from '@/components/posts/post-editor'
- 
-export const Route = createFileRoute('/posts/editor/$postId')({
+
+export const Route = createFileRoute('/posts/editor/')({
   component: RouteComponent,
 })
- 
+
 function RouteComponent() {
-  const { postId } = Route.useParams()
-  return <PostEditor postId={postId} />
+  return <PostEditor />
 }
- 
