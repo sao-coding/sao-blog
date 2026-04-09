b60241b1689b24ede031e2d79f6836c665b40f62
sao-coding <jeter.nice@gmail.com>
2026-04-09 23:02:21 +0800

    refactor: 更新 createApiResponseSchema，使用 discriminatedUnion 來處理成功和錯誤狀態

--- packages/api/src/schema/api.ts
+++ packages/api/src/schema/api.ts
@@ -7,9 +7,17 @@ export const createApiResponseSchema = <
   dataSchema: T,
   metaSchema?: M
 ) =>
-  z.object({
-    status: z.enum(["success", "error"]),
-    message: z.string(),
-    meta: metaSchema ? metaSchema.optional() : z.undefined().optional(),
-    data: dataSchema,
-  });
+  z.discriminatedUnion("status", [
+    z.object({
+      status: z.literal("success"),
+      message: z.string(),
+      meta: metaSchema ? metaSchema.optional() : z.undefined().optional(),
+      data: dataSchema,
+    }),
+    z.object({
+      status: z.literal("error"),
+      message: z.string(),
+      meta: metaSchema ? metaSchema.optional() : z.undefined().optional(),
+      data: z.null(),
+    }),
+  ]);
