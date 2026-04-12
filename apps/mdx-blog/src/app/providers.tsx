6b6d8d74cbbb3afad3bf5f7efc0a96b6705cc1a0
sao-coding <jeter.nice@gmail.com>
2026-04-13 02:16:00 +0800

    fix: 使用 orpc QueryClient，修復不同實例造成的問題

--- apps/mdx-blog/src/app/providers.tsx
+++ apps/mdx-blog/src/app/providers.tsx
@@ -1,12 +1,11 @@
 "use client";
 
 import { ModalProvider } from "@/components/providers/modal-provider";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { queryClient } from "@/lib/orpc";
+import { QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
-import { useState } from "react";
 
 const Providers = ({ children }: { children: React.ReactNode }) => {
-  const [queryClient] = useState(() => new QueryClient());
 
   return (
     <>
