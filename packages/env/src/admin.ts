2770883d438e04f15072af2195715fa5f13e9004
sao-coding <jeter.nice@gmail.com>
2026-05-24 23:27:23 +0800

    feat: 修正 runtimeEnv 設定以使用 import.meta.env

--- packages/env/src/admin.ts
+++ packages/env/src/admin.ts
@@ -2,14 +2,11 @@ import { createEnv } from "@t3-oss/env-core";
 import { z } from "zod";
 
 export const env = createEnv({
-	clientPrefix: "VITE_",
-	client: {
-		VITE_API_URL: z.url(),
-		VITE_BLOG_URL: z.url(),
-	},
-	runtimeEnv: {
-		VITE_API_URL: process.env.VITE_API_URL,
-		VITE_BLOG_URL: process.env.VITE_BLOG_URL,
-	},
-	emptyStringAsUndefined: true,
+  clientPrefix: "VITE_",
+  client: {
+    VITE_API_URL: z.url(),
+    VITE_BLOG_URL: z.url(),
+  },
+  runtimeEnv: import.meta.env,
+  emptyStringAsUndefined: true,
 });
