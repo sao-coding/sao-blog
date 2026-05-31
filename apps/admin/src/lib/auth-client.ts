f724b6c3688014ebbd39d1cc932482ff317d08c8
jeterluan <jeterluan@msi.com>
2026-05-31 14:36:41 +0800

    feat: 更新認證客戶端配置，新增插件以支援使用者名稱及 API 金鑰

--- apps/admin/src/lib/auth-client.ts
+++ apps/admin/src/lib/auth-client.ts
@@ -1,7 +1,9 @@
 import { createAuthClient } from "better-auth/react"
+import { apiKeyClient, usernameClient } from "better-auth/client/plugins";
 import { env } from "@sao-blog/env/admin";
 
 export const authClient = createAuthClient({
     /** The base URL of the server (optional if you're using the same domain) */
-    baseURL: env.VITE_API_URL
+    baseURL: env.VITE_API_URL,
+    plugins: [usernameClient(), apiKeyClient()],
 })
