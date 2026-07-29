91257873086b9c5014c82829bbeb19a1f6b223c2
jeterluan <jeterluan@msi.com>
2026-07-29 16:55:19 +0800

    chore: remove web project

--- apps/web/next.config.ts
+++
@@ -1,9 +0,0 @@
-import "@sao-blog/env/web";
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  typedRoutes: true,
-  reactCompiler: true,
-};
-
-export default nextConfig;
